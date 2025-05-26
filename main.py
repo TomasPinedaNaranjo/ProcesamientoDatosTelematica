6087424fcfeb871a3a4c1b49d7d2dd6e4f226c0c
TomasPinedaNaranjo <104210520+TomasPinedaNaranjo@users.noreply.github.com>
2025-05-26 15:03:48 -0500

    fix: path

--- main.py
+++ main.py
@@ -13,11 +13,9 @@ def main():
     filename = bot.save_to_file(productos, filename="todos_los_productos.txt")
 
     # Subir archivo a S3 usando upload_file.py
-    # Cambia los parámetros si es necesario
-    upload_file.file_path = filename
-    upload_file.s3_key = f"carpeta/{filename}"
-    upload_file.s3.upload_file(upload_file.file_path, upload_file.bucket_name, upload_file.s3_key)
-    print(f"Archivo {filename} subido exitosamente a {upload_file.bucket_name}/{upload_file.s3_key}")
+    s3_key = f"raw/{filename}"  # O la ruta que desees
+    upload_file.upload_to_s3(filename, upload_file.bucket_name, s3_key)
+    print(f"Archivo {filename} subido exitosamente a {upload_file.bucket_name}/{s3_key}")
 
 if __name__ == "__main__":
     main()
